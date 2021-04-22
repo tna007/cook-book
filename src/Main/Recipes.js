5c329cf072cc19750d11f7a92e453f381a7ab9a7
tna007 <anh.to@edu.bc.fi>
2021-04-22 16:31:50 +0300

    Add recipes list component

--- src/Main/Recipes.js
+++ src/Main/Recipes.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Search from "./Search";
+import RecipesList from "./RecipesList";
 
-const Recipes = () => {
-  return (
-    <section id="recipes">
-      <div>this is recipes</div>
-    </section>
-  );
-};
+class Recipes extends Component {
+  state = {
+    searchInput: "",
+    recipes: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3001/recipes")
+      .then((resp) => resp.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          recipes: json,
+        });
+        console.log(this.state.recipes);
+      });
+  }
+  searchHandler = (e) => {
+    this.setState({
+      searchInput: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <section className="recipes">
+        <Search search={this.searchHandler} />
+        <p>{this.state.searchInput}</p>
+        <RecipesList recipes={this.state.recipes} />
+      </section>
+    );
+  }
+}
 
 export default Recipes;
